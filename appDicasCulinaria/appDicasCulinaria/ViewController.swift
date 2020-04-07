f3e6c3ca3e9de1960e99586da238954305e4395e
FelipeNishino <49385633+FelipeNishino@users.noreply.github.com>
2020-04-07 15:22:29 -0300

    Criação do projeto
    Co-Authored-By: ju-machado <ju-machado@users.noreply.github.com>
    Co-Authored-By: frostmarchesan <frostmarchesan@users.noreply.github.com>
    Co-Authored-By: alex-andre-dev <alex-andre-dev@users.noreply.github.com>

---
+++ appDicasCulinaria/appDicasCulinaria/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  appDicasCulinaria
+//
+//  Created by Felipe Nishino on 07/04/20.
+//  Copyright © 2020 Felipe Nishino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
