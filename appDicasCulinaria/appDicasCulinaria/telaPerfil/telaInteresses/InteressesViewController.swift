23a6a7919b230c36c7e05cfa8abb89adb3a2b97e
FelipeNishino <49385633+FelipeNishino@users.noreply.github.com>
2020-05-01 19:13:45 -0300

    Funcionalidade telaInteresses
    Co-Authored-By: ju-machado <ju-machado@users.noreply.github.com>

--- appDicasCulinaria/appDicasCulinaria/telaPerfil/telaInteresses/InteressesViewController.swift
+++ appDicasCulinaria/appDicasCulinaria/telaPerfil/telaInteresses/InteressesViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 
 enum Interests : Int {
-    case empty
     case carnePerfeita
     case soVeggies
     case procedimentosBasicos
@@ -21,14 +20,6 @@ enum Interests : Int {
     case preAquecimento
     case massas
 }
-
-//UserDefaults.standard.set(Interests.empty.rawValue, forKey: "tagsInterests")
-//
-//let rawValue = UserDefaults.standard.integer(forKey: "tagsInterests")
-//
-//let tagsInterests = Interests(rawValue: rawValue) ?? .empty
-
-
 
 class interestsViewController: UIViewController {
     
@@ -43,186 +34,65 @@ class interestsViewController: UIViewController {
     @IBOutlet weak var btn_PreAquecimento: UIButton!
     @IBOutlet weak var btn_Massas: UIButton!
     
-    var tagSelected = Interests.empty
+    var setInterests = UserDefaults.Interests
+    let tempSet = UserDefaults.Interests
+
     
     let saveButton  = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saveInterest))
-    let cancelButton  = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(saveInterest))
+    
+    //    let cancelButton  = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(saveInterest))
     
     // var buttonsInterests = [UIButton]()
- 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        checkClicked()
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(saveInterest))
+        self.navigationItem.rightBarButtonItem!.isEnabled = false
+                
+        btn_SoVeggies.tag = Interests.soVeggies.rawValue
+        btn_CarnePerfeita.tag = Interests.carnePerfeita.rawValue
+        btn_DiaDia.tag = Interests.diaDia.rawValue
+        btn_Massas.tag = Interests.massas.rawValue
+        btn_ComoCortar.tag = Interests.comoCortar.rawValue
+        btn_MetodosCoccao.tag = Interests.metodosCoccao.rawValue
+        btn_Substituicoes.tag = Interests.substituicoes.rawValue
+        btn_PreAquecimento.tag = Interests.preAquecimento.rawValue
+        btn_VistosRecentemente.tag = Interests.vistosRecentemente.rawValue
+        btn_ProcedimentosBasicos.tag = Interests.procedimentosBasicos.rawValue
+        
+        getInterests()
+    }
+    
+    @objc func getInterests() {
+        print(tempSet.debugDescription)
+        
+        for item in tempSet {
+            let tempBtn = self.view.viewWithTag(item) as? UIButton
+            selectInterest(tempBtn!)
+        }
     }
     
     @objc func saveInterest() {
-        print("salvando...")
-        //checkClicked()
+        UserDefaults.Interests = setInterests
     }
     
-    func checkClicked(){
-        if tagSelected != Interests.empty{
-            
-            self.navigationItem.rightBarButtonItem = saveButton
-            self.navigationItem.leftBarButtonItem = cancelButton
-            
-            switch tagSelected{
-                
-            case .carnePerfeita:
-                if btn_CarnePerfeita.backgroundColor != #colorLiteral(red: 0.3575409949, green: 0.8815410733, blue: 0.5376247764, alpha: 1){
-                    btn_CarnePerfeita.backgroundColor = #colorLiteral(red: 0.3575409949, green: 0.8815410733, blue: 0.5376247764, alpha: 1)
-                    print("carne perfeita selecionada")
-                }else{
-                    btn_CarnePerfeita.backgroundColor = .systemGray6
-                    print("carne perfeita desselecionada")
-                }
-                
-            case .soVeggies:
-                if btn_SoVeggies.backgroundColor != #colorLiteral(red: 0.3575409949, green: 0.8815410733, blue: 0.5376247764, alpha: 1){
-                    btn_SoVeggies.backgroundColor = #colorLiteral(red: 0.3575409949, green: 0.8815410733, blue: 0.5376247764, alpha: 1)
-                    print("só veggies selecionada")
-                }else{
-                    btn_SoVeggies.backgroundColor = .systemGray2
-                    print("só veggies desselecionada")
-                }
-                
-            case .procedimentosBasicos:
-                if btn_ProcedimentosBasicos.backgroundColor != #colorLiteral(red: 0.3575409949, green: 0.8815410733, blue: 0.5376247764, alpha: 1){
-                    btn_ProcedimentosBasicos.backgroundColor = #colorLiteral(red: 0.3575409949, green: 0.8815410733, blue: 0.5376247764, alpha: 1)
-                    print("procedimentos básicos selecionada")
-                }else{
-                    btn_ProcedimentosBasicos.backgroundColor = .systemGray3
-                    print("procedimentos básicos desselecionada")
-                }
-                
-            case .metodosCoccao:
-                if btn_MetodosCoccao.backgroundColor != #colorLiteral(red: 0.3575409949, green: 0.8815410733, blue: 0.5376247764, alpha: 1){
-                    btn_MetodosCoccao.backgroundColor = #colorLiteral(red: 0.3575409949, green: 0.8815410733, blue: 0.5376247764, alpha: 1)
-                    print("métodos de cocção selecionada")
-                }else{
-                    btn_MetodosCoccao.backgroundColor = .systemGray6
-                    print("métodos de cocção desselecionada")
-                }
-                
-            case .diaDia:
-                if btn_DiaDia.backgroundColor != #colorLiteral(red: 0.3575409949, green: 0.8815410733, blue: 0.5376247764, alpha: 1){
-                    btn_DiaDia.backgroundColor = #colorLiteral(red: 0.3575409949, green: 0.8815410733, blue: 0.5376247764, alpha: 1)
-                    print("dia a dia selecionada")
-                }else{
-                    btn_DiaDia.backgroundColor = .systemGray2
-                    print("dia a dia desselecionada")
-                }
-                
-            case .substituicoes:
-                if btn_Substituicoes.backgroundColor != #colorLiteral(red: 0.3575409949, green: 0.8815410733, blue: 0.5376247764, alpha: 1){
-                    btn_Substituicoes.backgroundColor = #colorLiteral(red: 0.3575409949, green: 0.8815410733, blue: 0.5376247764, alpha: 1)
-                    print("substituições selecionada")
-                }else{
-                    btn_Substituicoes.backgroundColor = .systemGray3
-                    print("substituições desselecionada")
-                }
-                
-            case .comoCortar:
-                if btn_ComoCortar.backgroundColor != #colorLiteral(red: 0.3575409949, green: 0.8815410733, blue: 0.5376247764, alpha: 1){
-                    btn_ComoCortar.backgroundColor = #colorLiteral(red: 0.3575409949, green: 0.8815410733, blue: 0.5376247764, alpha: 1)
-                    print("como cortar selecionada")
-                }else{
-                    btn_ComoCortar.backgroundColor = .systemGray4
-                    print("como cortar desselecionada")
-                }
-                
-            case .vistosRecentemente:
-                if btn_VistosRecentemente.backgroundColor != #colorLiteral(red: 0.3575409949, green: 0.8815410733, blue: 0.5376247764, alpha: 1){
-                    btn_VistosRecentemente.backgroundColor = #colorLiteral(red: 0.3575409949, green: 0.8815410733, blue: 0.5376247764, alpha: 1)
-                    print("visto recentemente selecionada")
-                }else{
-                    btn_VistosRecentemente.backgroundColor = .systemGray3
-                    print("visto recentemente desselecionada")
-                }
-                
-            case .preAquecimento:
-                if btn_PreAquecimento.backgroundColor != #colorLiteral(red: 0.3575409949, green: 0.8815410733, blue: 0.5376247764, alpha: 1){
-                    btn_PreAquecimento.backgroundColor = #colorLiteral(red: 0.3575409949, green: 0.8815410733, blue: 0.5376247764, alpha: 1)
-                    print("pré-aquecimento selecionada")
-                }else{
-                    btn_PreAquecimento.backgroundColor = .systemGray2
-                    print("pré-aquecimento desselecionada")
-                }
-                
-            case .massas:
-                if btn_Massas.backgroundColor != #colorLiteral(red: 0.3575409949, green: 0.8815410733, blue: 0.5376247764, alpha: 1){
-                    btn_Massas.backgroundColor = #colorLiteral(red: 0.3575409949, green: 0.8815410733, blue: 0.5376247764, alpha: 1)
-                    print("massas selecionada")
-                }else{
-                    btn_Massas.backgroundColor = .systemGray4
-                    print("massas desselecionada")
-                }
-                
-            default:
-                print(tagSelected)
-            }
+        
+    @IBAction func selectInterest(_ sender: UIButton) {
+        if sender.backgroundColor != #colorLiteral(red: 0.3575409949, green: 0.8815410733, blue: 0.5376247764, alpha: 1){
+            sender.backgroundColor = #colorLiteral(red: 0.3575409949, green: 0.8815410733, blue: 0.5376247764, alpha: 1)
+            setInterests.insert(sender.tag)
+        }
+        else {
+            sender.backgroundColor = .systemGray6
+            setInterests.remove(sender.tag)
         }
         
-        
+        if !setInterests.symmetricDifference(tempSet).isEmpty {
+            self.navigationItem.rightBarButtonItem!.isEnabled = true
+        }
+        else {
+            self.navigationItem.rightBarButtonItem!.isEnabled = false
+        }
     }
-    
-    
-    
-    
-    @IBAction func btn_CarnePerfeita(_ sender: UIButton) {
-        print("carne")
-        tagSelected = Interests.carnePerfeita
-        checkClicked()
-        
-        // buttonsInterests.append(btn_CarnePerfeita)
-    }
-    
-    @IBAction func btn_SoVeggies(_ sender: UIButton) {
-        print("veggie")
-        tagSelected = Interests.soVeggies
-        checkClicked()
-    }
-    
-    @IBAction func btn_ProcedimentosBasicos(_ sender: UIButton) {
-        tagSelected = Interests.procedimentosBasicos
-        checkClicked()
-    }
-    
-    @IBAction func btn_MetodosCoccao(_ sender: UIButton) {
-        tagSelected = Interests.metodosCoccao
-        checkClicked()
-    }
-    
-    @IBAction func btn_DiaDia(_ sender: UIButton) {
-        tagSelected = Interests.diaDia
-        checkClicked()
-    }
-    
-    @IBAction func btn_Substituicoes(_ sender: UIButton) {
-        tagSelected = Interests.substituicoes
-        checkClicked()
-    }
-    
-    @IBAction func btn_ComoCortar(_ sender: UIButton) {
-        tagSelected = Interests.comoCortar
-        checkClicked()
-    }
-    
-    @IBAction func btn_VistosRecentemente(_ sender: UIButton) {
-        tagSelected = Interests.vistosRecentemente
-        checkClicked()
-    }
-    
-    @IBAction func btn_PreAquecimento(_ sender: UIButton) {
-        tagSelected = Interests.preAquecimento
-        checkClicked()
-    }
-    
-    @IBAction func btn_Massas(_ sender: UIButton) {
-        tagSelected = Interests.massas
-        checkClicked()
-    }
-    
-    
 }
 
